b070a059debfa8ec7eea7598d1d1c727d234d2db
envyvox <kkxo.mail@gmail.com>
2022-05-20 15:25:42 +0300

    Since nadeko updated his embed builder and i'm not, replacing mention to old builder

--- Vox.Services/Discord/Interactions/Modals/SendEmbedModal.cs
+++ Vox.Services/Discord/Interactions/Modals/SendEmbedModal.cs
@@ -8,6 +8,6 @@ public class SendEmbedModal : IModal
     public string Title => "Send embed";
 
     [InputLabel("Generated JSON code")]
-    [ModalTextInput("json-code", TextInputStyle.Paragraph, "Generated JSON code from eb.nadeko.bot")]
+    [ModalTextInput("json-code", TextInputStyle.Paragraph, "Generated JSON code from oldeb.nadeko.bot")]
     public string JsonCode { get; set; }
 }
